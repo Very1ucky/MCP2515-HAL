fa17bebbf573d7dad51be73b91fa0ebe4de0c7d2
Daniil <sukhikhDaniilA@yandex.ru>
2023-11-22 21:22:37 +0300

    Change can_frame_t, write func to conv frame to regs

--- Core/Inc/can.h
+++ Core/Inc/can.h
@@ -1,6 +1,15 @@
 #ifndef CAN_H_
 #define CAN_H_
 
+#include "mcp2515.h"
 
+typedef struct {
+    uint16_t st_id;
+    uint32_t ext_id;
+    bool is_extended;
+    bool is_rtr;
+    uint8_t dlc;
+    uint8_t data[8];
+} can_frame_t;
 
 #endif // CAN_H_
--- Core/Src/can.c
+++ Core/Src/can.c
@@ -1,2 +1,37 @@
 #include "can.h"
 
+void can_init()
+{
+}
+
+void conv_can_frame_2_regs(can_frame_t *frame, tx_rx_reg_packet_t *regs)
+{
+    regs->TXRXBnSIDH = frame->st_id >> 3;
+    regs->TXRXBnSIDL = frame->st_id << 5 && 0xE0;
+
+    if (frame->is_extended)
+    {
+        // set extended identifier enable
+        regs->TXRXBnSIDL |= 0x8;
+
+        regs->TXRXBnSIDL |= frame->ext_id >> 16;
+
+        regs->TXRXBnEID8 = frame->ext_id >> 8;
+        regs->TXRXBnEID0 = frame->ext_id;
+    }
+    else
+    {
+        regs->TXRXBnEID8 = 0;
+        regs->TXRXBnEID0 = 0;
+    }
+
+    regs->TXRXBnDLC = frame->dlc && 0xF;
+
+    if (frame->is_rtr) {
+        regs->TXRXBnDLC |= 0x40;
+    }
+
+    for (int i = 0; i < frame->dlc; ++i) {
+        regs->TXRXBnDx[i] = frame->data[i];
+    }
+}
