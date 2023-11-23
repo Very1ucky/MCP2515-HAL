47b768b30c7c7768c053bf6a2f23c0783b4f0a45
Daniil <sukhikhDaniilA@yandex.ru>
2023-11-23 16:48:37 +0300

    Write func to recieve messages, also add process_status_t as return

--- Core/Inc/can.h
+++ Core/Inc/can.h
@@ -6,10 +6,10 @@
 #define TX_BUF_COUNT 3
 #define RX_BUF_COUNT 2
 
-#define IS_BUF_FREE(__BUF_NUM__, __STATUS__) 1<<(2*(__BUF_NUM__))&__STATUS__==0
+#define IS_TX_BUF_FREE(__BUF_NUM__, __STATUS__) (1<<(2*(__BUF_NUM__)))&(__STATUS__)==0
 
-#define IS_FRAME_EXTENDED(__FRAME__) __FRAME__->frame_type&0x2
-#define IS_FRAME_RMT(__FRAME__) __FRAME__->frame_type&0x1
+#define IS_FRAME_EXTENDED(__FRAME__) __FRAME__->type&0x2
+#define IS_FRAME_RMT(__FRAME__) __FRAME__->type&0x1
 
 typedef enum {
     ST_DATA = 0x0,
@@ -21,7 +21,7 @@ typedef enum {
 typedef struct {
     uint16_t st_id;
     uint32_t ext_id;
-    can_frame_type_t frame_type;
+    can_frame_type_t type;
     uint8_t dlc;
     uint8_t data[8];
 } can_frame_t;
--- Core/Src/can.c
+++ Core/Src/can.c
@@ -1,44 +1,62 @@
 #include "can.h"
 
 static void conv_can_frame_2_regs(can_frame_t *frame, tx_rx_reg_packet_t *regs);
-static conv_regs_2_can_frame(can_frame_t *frame, tx_rx_reg_packet_t *regs, can_frame_type_t frame_type);
+static void conv_regs_2_can_frame(can_frame_t *frame, tx_rx_reg_packet_t *regs, can_frame_type_t frame_type);
 static uint8_t get_first_free_tx_buf();
 static uint8_t get_rx_full_buf(can_frame_type_t *frame_type);
 
-void send_can_frame(can_frame_t *frame)
+process_status_t send_can_frame(can_frame_t *frame)
 {
     static tx_rx_reg_packet_t tx_regs;
 
     conv_can_frame_2_regs(frame, &tx_regs);
 
     uint8_t free_buf_num = get_first_free_tx_buf();
-    mcp2515_write_tx_buffer(free_buf_num, (uint8_t *)&tx_regs,
+    if (free_buf_num == 0)
+    {
+        return NO_FREE_TX_BUF;
+    }
+
+    return mcp2515_write_tx_buffer(free_buf_num, (uint8_t *)&tx_regs,
                             sizeof(tx_rx_reg_packet_t), false);
 }
 
-void recieve_can_frame(can_frame_t *frame)
+process_status_t recieve_can_frame(can_frame_t *frame)
 {
     static tx_rx_reg_packet_t rx_regs;
 
+    process_status_t status;
+
     can_frame_type_t frame_type;
     uint8_t full_buf_num = get_rx_full_buf(&frame_type);
-    mcp2515_read_rx_buffer(full_buf_num, (uint8_t *)&rx_regs,
+    if (full_buf_num == 0)
+    {
+        return NO_FULL_RX_BUF;
+    }
+
+    status = mcp2515_read_rx_buffer(full_buf_num, (uint8_t *)&rx_regs,
                            sizeof(tx_rx_reg_packet_t), false);
 
     conv_regs_2_can_frame(frame, &rx_regs, frame_type);
+
+    return status;
 }
 
 static uint8_t get_rx_full_buf(can_frame_type_t *frame_type)
 {
-    static status;
+    static uint8_t status;
 
     mcp2515_get_rx_status(&status);
 
-    for (int buf_num = 0; buf_num < TX_BUF_COUNT; ++buf_num)
+    *frame_type = (can_frame_type_t)((status >> 3) & 0x3);
+
+    for (int buf_num = 1; buf_num <= RX_BUF_COUNT; ++buf_num)
     {
-        if (IS_BUF_FREE(buf_num, status))
+        if (READ_BIT(status, buf_num + 1))
             return buf_num;
     }
+
+    return 0;
 }
 
 static uint8_t get_first_free_tx_buf()
@@ -48,17 +66,19 @@ static uint8_t get_first_free_tx_buf()
 
     mcp2515_get_read_status(&status);
 
-    for (int buf_num = 0; buf_num < TX_BUF_COUNT; ++buf_num)
+    for (int buf_num = 1; buf_num <= TX_BUF_COUNT; ++buf_num)
     {
-        if (IS_BUF_FREE(buf_num, status))
+        if (IS_TX_BUF_FREE(buf_num, status))
             return buf_num;
     }
+
+    return 0;
 }
 
 static void conv_can_frame_2_regs(can_frame_t *frame, tx_rx_reg_packet_t *regs)
 {
     regs->TXRXBnSIDH = frame->st_id >> 3;
-    regs->TXRXBnSIDL = frame->st_id << 5 && 0xE0;
+    regs->TXRXBnSIDL = (frame->st_id << 5) & 0xE0;
 
     if (IS_FRAME_EXTENDED(frame))
     {
@@ -89,9 +109,22 @@ static void conv_can_frame_2_regs(can_frame_t *frame, tx_rx_reg_packet_t *regs)
     }
 }
 
-static conv_regs_2_can_frame(can_frame_t *frame, tx_rx_reg_packet_t *regs, can_frame_type_t frame_type)
+static void conv_regs_2_can_frame(can_frame_t *frame, tx_rx_reg_packet_t *regs, can_frame_type_t frame_type)
 {
-    
+    frame->st_id = regs->TXRXBnSIDH << 3 | regs->TXRXBnSIDL >> 5;
+    frame->type = frame_type;
+
+    if (IS_FRAME_EXTENDED(frame))
+    {
+        frame->ext_id = (regs->TXRXBnSIDL & 0x3) | regs->TXRXBnEID8 << 8 |
+                        regs->TXRXBnEID0;
+    }
+
+    frame->dlc = regs->TXRXBnDLC & 0xF;
+    for (int i = 0; i < frame->dlc; ++i)
+    {
+        frame->data[i] = regs->TXRXBnDx[i];
+    }
 }
 
 void can_init()
