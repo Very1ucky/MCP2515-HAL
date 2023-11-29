f454309aed64a197ef3bca55d2acfbfca729017e
Daniil <sukhikhDaniilA@yandex.ru>
2023-11-29 19:32:11 +0300

    Add gyroscope sensor

--- Core/Inc/accel_amperka.h
+++ Core/Inc/accel_amperka.h
@@ -7,28 +7,26 @@
 #include "proc_status.h"
 
 #define LIS331DLN_ADDR 0x30
+#define L3G4200D_ADDR 0xD0
 #define I2C_INT hi2c1
 
-#define LIS331DLN_WHO_AM_I_REG_ADDR 0x0F
-#define LIS331DLN_WHO_AM_I_REG_DATA 0b00110010
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
 
-#define LIS331DLN_CTRL_REG1 0x20
-#define LIS331DLN_CTRL_REG2 0x21
-#define LIS331DLN_CTRL_REG3 0x22
-#define LIS331DLN_CTRL_REG4 0x23
-#define LIS331DLN_CTRL_REG5 0x24
+#define HP_FILTER_RESET 0x25
 
-#define LIS331DLN_HP_FILTER_RESET 0x25
-
-#define LIS331DLN_OUT_X_L 0x28
-#define LIS331DLN_OUT_X_H 0x29
-#define LIS331DLN_OUT_Y_L 0x2A
-#define LIS331DLN_OUT_Y_H 0x2B
-#define LIS331DLN_OUT_Z_L 0x2C
-#define LIS331DLN_OUT_Z_H 0x2D
+#define OUT_X_L 0x28
+#define OUT_X_H 0x29
+#define OUT_Y_L 0x2A
+#define OUT_Y_H 0x2B
+#define OUT_Z_L 0x2C
+#define OUT_Z_H 0x2D
 
 // set MSB bit to 1 for multireg reading
-#define LIS331DLN_OUT_XYZ_START LIS331DLN_OUT_X_L|0x80
+#define OUT_XYZ_START OUT_X_L|0x80
 /* enable HPF on data
 */
 #define LIS331DLN_CTRL_REG2_DATA 0b10010000
@@ -38,6 +36,14 @@
 #define LIS331DLN_CTRL_REG4_DATA 0b00000000
 
 #define LIS331DLN_CTRL_REG5_DATA 0b00000000
+
+
+#define L3G4200D_CTRL_REG1_DATA 0b00001111
+#define L3G4200D_CTRL_REG2_DATA 0b00000000
+#define L3G4200D_CTRL_REG3_DATA 0b00000000
+// enable BDU (scale 250 dps)
+#define L3G4200D_CTRL_REG4_DATA 0b10000000
+#define L3G4200D_CTRL_REG5_DATA 0b00000000
 
 typedef enum
 {
@@ -56,11 +62,10 @@ typedef struct {
     int16_t X;
     int16_t Y;
     int16_t Z;
-} acel_data_t;
+} axes_sensor_data_t;
 
-process_status_t read_velocity(acel_data_t *data_frame);
+process_status_t read_axes_sensor_data(uint16_t dev_addr,axes_sensor_data_t *data_frame);
 process_status_t LIS331DLN_init(acel_poll_rate_t poll_rate);
-process_status_t LIS331DLN_turn_on(acel_poll_rate_t poll_rate);
-process_status_t LIS331DLN_calibrate();
+process_status_t L3G4200D_init();
 
 #endif // ACCEL_AMPERKA_H_
--- Core/Src/accel_amperka.c
+++ Core/Src/accel_amperka.c
@@ -2,54 +2,42 @@
 
 extern I2C_HandleTypeDef I2C_INT;
 
-process_status_t I2C_read_byte(uint16_t addr, uint8_t *data);
-process_status_t I2C_write_byte(uint16_t addr, uint8_t data);
+process_status_t I2C_read_byte(uint16_t dev_addr, uint16_t addr, uint8_t *data);
+process_status_t I2C_write_byte(uint16_t dev_addr, uint16_t addr, uint8_t data);
 
-process_status_t I2C_read_buffer(uint16_t addr, uint8_t *data, uint8_t length);
-process_status_t I2C_write_buffer(uint16_t addr, uint8_t *data, uint8_t length);
+process_status_t I2C_read_buffer(uint16_t dev_addr, uint16_t addr, uint8_t *data, uint8_t length);
+process_status_t I2C_write_buffer(uint16_t dev_addr, uint16_t addr, uint8_t *data, uint8_t length);
 
 process_status_t LIS331DLN_init(acel_poll_rate_t poll_rate)
 {
     process_status_t status = OK;
 
-    status = LIS331DLN_turn_on(poll_rate);
+    status = I2C_write_byte(LIS331DLN_ADDR, CTRL_REG1, poll_rate);
 
-    if (status)
+    HAL_Delay(21);
+    acel_poll_rate_t set_poll_rate;
+    status = I2C_read_byte(LIS331DLN_ADDR, CTRL_REG1, (uint8_t *)(&set_poll_rate));
+    if (set_poll_rate != poll_rate)
     {
-        return status;
+        return FAILED;
     }
 
     // set ctrl_regs 2,3,4,5 data
     uint8_t ctrl_regs_data[] = {LIS331DLN_CTRL_REG2_DATA, LIS331DLN_CTRL_REG3_DATA,
                                 LIS331DLN_CTRL_REG4_DATA, LIS331DLN_CTRL_REG5_DATA};
 
-    status = I2C_write_buffer(LIS331DLN_CTRL_REG2 | 0x80, ctrl_regs_data, 4);
+    status = I2C_write_buffer(LIS331DLN_ADDR, CTRL_REG2, ctrl_regs_data, 4);
+
+    if (status)
+    {
+        return status;
+    }
 
     /* dummy read to force the HP filter to actual acceleration value (set reference acceleretion),
     if needed (set zero poz of acelerometr)
     */
     uint8_t dummy;
-    status = I2C_read_buffer(LIS331DLN_HP_FILTER_RESET, &dummy, 1);
-
-
-    return status;
-}
-
-process_status_t LIS331DLN_turn_on(acel_poll_rate_t poll_rate)
-{
-    process_status_t status = OK;
-
-    status = I2C_write_byte(LIS331DLN_CTRL_REG1, poll_rate);
-
-    // wait turn on time (AN2847)
-    HAL_Delay(21);
-    acel_poll_rate_t set_poll_rate;
-    status = I2C_read_byte(LIS331DLN_CTRL_REG1, (uint8_t *)(&set_poll_rate));
-
-    if (set_poll_rate != poll_rate)
-    {
-        return FAILED;
-    }
+    status = I2C_read_buffer(LIS331DLN_ADDR, HP_FILTER_RESET, &dummy, 1);
 
     return status;
 }
@@ -57,42 +45,39 @@ process_status_t LIS331DLN_turn_on(acel_poll_rate_t poll_rate)
 process_status_t LIS331DLN_turn_off()
 {
     process_status_t status = OK;
-    status = I2C_write_byte(LIS331DLN_CTRL_REG1, 0x00);
+    status = I2C_write_byte(LIS331DLN_ADDR, CTRL_REG1, 0x00);
 
     return status;
 }
 
-process_status_t LIS331DLN_calibrate()
-{
-    process_status_t status = OK;
-    uint8_t ctrl_reg2_data = 0;
-
-    I2C_read_byte(LIS331DLN_CTRL_REG2, &ctrl_reg2_data);
-    // active HPF on data
-    ctrl_reg2_data |= 0x10;
-    status = I2C_write_byte(LIS331DLN_CTRL_REG2, ctrl_reg2_data);
-
-    uint8_t dummy;
-    status = I2C_read_byte(LIS331DLN_HP_FILTER_RESET, &dummy);
-
-    return status;
-}
-
-process_status_t read_velocity(acel_data_t *data_frame)
+process_status_t read_axes_sensor_data(uint16_t dev_addr, axes_sensor_data_t *data_frame)
 {
     process_status_t status = OK;
 
-    status = I2C_read_buffer(LIS331DLN_OUT_XYZ_START, (uint8_t *)data_frame, 6);
+    status = I2C_read_buffer(dev_addr, OUT_XYZ_START, (uint8_t *)data_frame, 6);
 
     return status;
 }
 
-process_status_t I2C_read_byte(uint16_t addr, uint8_t *data)
+process_status_t L3G4200D_init()
 {
-    return I2C_read_buffer(addr, data, 1);
+    process_status_t status = OK;
+
+    // set ctrl_regs  data
+    uint8_t ctrl_regs_data[] = {L3G4200D_CTRL_REG1_DATA, L3G4200D_CTRL_REG2_DATA, L3G4200D_CTRL_REG3_DATA,
+                                L3G4200D_CTRL_REG4_DATA, L3G4200D_CTRL_REG5_DATA};
+
+    status = I2C_write_buffer(L3G4200D_ADDR, CTRL_REG1, ctrl_regs_data, 5);
+
+    return status;
 }
 
-process_status_t I2C_read_buffer(uint16_t addr, uint8_t *data, uint8_t length)
+process_status_t I2C_read_byte(uint16_t dev_addr, uint16_t addr, uint8_t *data)
+{
+    return I2C_read_buffer(dev_addr, addr, data, 1);
+}
+
+process_status_t I2C_read_buffer(uint16_t dev_addr, uint16_t addr, uint8_t *data, uint8_t length)
 {
     // set MSB bit for multi reading
     if (length > 1)
@@ -100,7 +85,7 @@ process_status_t I2C_read_buffer(uint16_t addr, uint8_t *data, uint8_t length)
         addr |= 0x80;
     }
 
-    if (HAL_I2C_Mem_Read(&I2C_INT, LIS331DLN_ADDR, addr,
+    if (HAL_I2C_Mem_Read(&I2C_INT, dev_addr, addr,
                          1, data, length, 200))
     {
         return TRANSFER_ERROR;
@@ -109,12 +94,12 @@ process_status_t I2C_read_buffer(uint16_t addr, uint8_t *data, uint8_t length)
     return OK;
 }
 
-process_status_t I2C_write_byte(uint16_t addr, uint8_t data)
+process_status_t I2C_write_byte(uint16_t dev_addr, uint16_t addr, uint8_t data)
 {
-    return I2C_write_buffer(addr, &data, 1);
+    return I2C_write_buffer(dev_addr, addr, &data, 1);
 }
 
-process_status_t I2C_write_buffer(uint16_t addr, uint8_t *data, uint8_t length)
+process_status_t I2C_write_buffer(uint16_t dev_addr, uint16_t addr, uint8_t *data, uint8_t length)
 {
     // set MSB bit for multi writing
     if (length > 1)
@@ -122,7 +107,7 @@ process_status_t I2C_write_buffer(uint16_t addr, uint8_t *data, uint8_t length)
         addr |= 0x80;
     }
 
-    if (HAL_I2C_Mem_Write(&I2C_INT, LIS331DLN_ADDR, addr,
+    if (HAL_I2C_Mem_Write(&I2C_INT, dev_addr, addr,
                           1, data, length, 200))
     {
         return TRANSFER_ERROR;
